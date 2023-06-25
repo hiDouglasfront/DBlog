987fe2575fc7ab1083d690c2f81721a9897a29b5
hidouglasfront <hidougldasdigital@gmail.com>
2023-06-25 16:22:18 -0300

    Pequena correção na página Blog

--- src/pages/Blog/index.vue
+++ src/pages/Blog/index.vue
@@ -6,7 +6,7 @@ useAnimate(el, keyframes, 2000);
 
 <template>
   <main class="h-screen bg-[#0C0C0D] text-gray-100">
-    <section class="container mx-auto py-8 px-4 md:px-0"></section>
+    <section class="container mx-auto py-8 px-4 md:px-0">
       <h1 class="text-2xl font-bold mb-4">Blog</h1>
       <div ref="el">
         <BlogCardArticle />
